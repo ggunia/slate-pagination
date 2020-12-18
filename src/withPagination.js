cab1fd8ee271afd858a3051487529c1774814ce8
ggunia <guramgunia@gmail.com>
2020-12-18 14:48:45 +0400

    fix: remove console.logs

--- src/withPagination.js
+++ src/withPagination.js
@@ -26,7 +26,7 @@ const createNewPage = ({ editor, currPageIndex }) => {
   );
 
   Transforms.liftNodes(editor, { at: [currPageIndex, lastIndex] });
-  console.log(editor.children)
+
   const lastPageIndex = editor.children.length - 1
   const lastNodeIndex = editor
     .children[lastPageIndex]
@@ -34,8 +34,46 @@ const createNewPage = ({ editor, currPageIndex }) => {
     .length - 1
 
   Transforms.removeNodes(editor, { at: [lastPageIndex, lastNodeIndex] })
-  console.log('after removing node -> ', editor.children)
 };
+
+const splitNode = ({ editor, path, nodeText }) => {
+  Transforms.insertText(
+    editor,
+    nodeText.slice(0, nodeText.length - 10),
+    { at: path }
+  )
+  Transforms.insertNodes(
+    editor,
+    { type: 'paragraph', id: shortid.generate(), children: [{ text: nodeText.slice(nodeText.length - 10) }] },
+    { at: [path[0] + 1, 0] }
+  )
+  Transforms.removeNodes(
+    editor,
+    {
+      at: [
+        editor.children.length - 1,
+        editor.children[editor.children.length - 1].children.length - 1
+      ]
+    }
+  )
+  Transforms.setSelection(editor, {
+    anchor: {
+      path: [path[0] + 1, 0, 0],
+      offset: 10,
+    },
+    focus: {
+      path: [path[0] + 1, 0, 0],
+      offset: 10,
+    },
+  })
+}
+
+const getNodeText = ({ node }) => {
+  return node
+    .children
+    .map(curr => curr.text)
+    .reduce((acc, curr) => acc + curr, '')
+}
 
 const withPagination = rowMeasurer => (editor) => {
   const { normalizeNode } = editor;
@@ -52,7 +90,6 @@ const withPagination = rowMeasurer => (editor) => {
         const pageMaxHeight = 54
 
         const currPage = childPath[0]
-        const currNodeLine = childPath[1]
 
         if (height > pageMaxHeight) {
           const nextPage = editor.children[currPage + 1];
@@ -62,53 +99,17 @@ const withPagination = rowMeasurer => (editor) => {
             return;
           }
 
-          const node = editor
-            .children[childPath[0]]
-            .children[childPath[1]]
-          const nodeText = node
-            .children
-            .map(curr => curr.text)
-            .reduce((acc, curr) => acc + curr, '')
+          const nodeText = getNodeText({ node: child })
 
-          if (nodeText && rowMeasurer(node) > 18) {
-            Transforms.insertText(
-              editor,
-              nodeText.slice(0, nodeText.length - 10),
-              { at: childPath }
-            )
-            Transforms.insertNodes(
-              editor,
-              { type: 'paragraph', id: shortid.generate(), children: [{ text: nodeText.slice(nodeText.length - 10) }] },
-              { at: [childPath[0] + 1, 0] }
-            )
-            Transforms.removeNodes(
-              editor,
-              {
-                at: [
-                  editor.children.length - 1,
-                  editor.children[editor.children.length - 1].children.length - 1
-                ]
-              }
-            )
-            Transforms.setSelection(editor, {
-              anchor: {
-                path: [childPath[0] + 1, 0, 0],
-                offset: 10,
-              },
-              focus: {
-                path: [childPath[0] + 1, 0, 0],
-                offset: 10,
-              },
-            })
-          } else {
-            console.log('inside else case !!')
-            Transforms.moveNodes(editor, {
-              at: childPath,
-              to: [childPath[0] + 1, 0]
-            });
+          if (nodeText && rowMeasurer(child) > 18) {
+            splitNode({ editor, path: childPath, nodeText })
+            return
           }
 
-          console.log('after moving node -> ', editor.children)
+          Transforms.moveNodes(editor, {
+            at: childPath,
+            to: [childPath[0] + 1, 0]
+          });
           return
         }
       }
