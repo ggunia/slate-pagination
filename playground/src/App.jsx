c697a881a670a9fa08bea23b3adc33e58e8a6987
ggunia <guramgunia@gmail.com>
2020-12-19 15:47:57 +0400

    refactor: cleanup

--- playground/src/App.jsx
+++ playground/src/App.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React from "react";
+import { Slate, withReact, Editable } from "slate-react";
+import { createEditor } from "slate";
 
-const App = () => {
+import withPagination from "../../lib/withPaging";
+import Page from "./Page";
+
+const initialState = [
+  {
+    type: "page",
+    children: [
+      {
+        type: "paragraph",
+        children: [{ text: "Some" }]
+      },
+    ]
+  },
+];
+
+export default function App() {
+  const [editorState, updateEditorState] = React.useState(initialState);
+
+  const editor = React.useMemo(
+    () => withPagination(withReact(createEditor())),
+    []
+  );
+
+  const renderElement = React.useCallback(
+    ({ element, children, attributes }) => {
+      if (element.type === "page") {
+        return (
+          <Page id={element.id} content={children} attributes={attributes} />
+        );
+      }
+
+      return (
+        <p
+          data-node-id={element.id}
+          style={{ backgroundColor: 'lightblue', margin: 0 }}
+          {...attributes}
+        >
+          {children}
+        </p>
+      );
+    },
+    []
+  );
+
+  const onEditorChange = (value) => {
+    updateEditorState(value);
+  };
+
   return (
-    <div>Mdaa</div>
-  )
+    <div style={{ padding: 50, border: "1px solid black" }}>
+      <Slate
+        editor={editor}
+        value={editorState}
+        onChange={onEditorChange}
+        operations={[]}
+      >
+        <Editable renderElement={renderElement} />
+      </Slate>
+    </div>
+  );
 }
-
-export default App
