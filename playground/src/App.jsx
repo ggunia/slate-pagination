60a0333d97fefee74b7148e98fae233000829261
saba-coding <saba.chikhina@gmail.com>
2020-12-29 23:55:48 +0400

    feat: collaborative

--- playground/src/App.jsx
+++ playground/src/App.jsx
@@ -3,6 +3,8 @@ import { Slate, withReact, Editable } from 'slate-react'
 import { createEditor, Transforms } from 'slate'
 import { createGlobalStyle } from 'styled-components'
 import shortid from 'shortid'
+import * as sharedb from 'sharedb/lib/client'
+import * as jsondiff from 'json0-ot-diff'
 
 import withPagination from '../../lib/withPaging'
 import { getSelectedRange, selectRange } from './editorUtils'
@@ -73,13 +75,91 @@ const initialState = [
   },
 ]
 
+const userId = Date.now()
+
+const ws_client = new WebSocket('ws://localhost:8000/')
+const connection = new sharedb.Connection(ws_client)
+
+const doc = connection.get('sections', '0b3f31eb-c886-44fb-85d0-64b9cdfbdf27')
+const comp = connection.get('components', 'OxyAEV8qR')
+
 export default function App() {
+  const syncMutex = React.useRef()
+  const oldValue = React.useRef()
   const [editorState, updateEditorState] = React.useState(initialState)
+  console.log("🚀 ~ file: App.jsx ~ line 90 ~ App ~ editorState", editorState)
+  const [selections, setSelections] = React.useState([])
 
   const editor = React.useMemo(
     () => withPagination({})(withRectangleSelect(withNodeId(withReact(createEditor())))),
     []
   )
+
+  const oldSelection = React.useRef([{
+    id: userId,
+    selection: { anchor: { path: [0, 0], offset: 0 }, focus: { path: [0, 0], offset: 0 } }
+  }])
+
+  React.useEffect(() => {
+    const onSubscribe = () => {
+      syncMutex.current = true
+      updateEditorState(doc.data.children)
+      console.log("🚀 ~ file: App.jsx ~ line 167 ~ onSubscribe ~ doc.data.children", doc.data.children)
+      console.log('doc', doc.data)
+      syncMutex.current = false
+    }
+
+    const onCompSubscribe = () => {
+      syncMutex.current = true
+      
+      console.log('fffff', comp.data)
+      
+      syncMutex.current = false
+    }
+
+    const onOperation = () => {
+      syncMutex.current = true
+      const mySelection = doc
+        .data
+        .selections
+        .find(currSelection => currSelection.id === userId)
+      const otherSelections = doc
+        .data
+        .selections
+        .filter(currSelection => currSelection.id !== userId)
+
+      if (mySelection) editor.selection = mySelection.selection
+
+      setSelections(otherSelections)
+      updateEditorState(doc.data.children)
+      syncMutex.current = false
+    }
+
+    const onCompOperation = () => {
+      console.log('comp', comp.data.children)
+      // syncMutex.current = true
+      // const mySelection = doc
+      //   .data
+      //   .selections
+      //   .find(currSelection => currSelection.id === userId)
+      // const otherSelections = doc
+      //   .data
+      //   .selections
+      //   .filter(currSelection => currSelection.id !== userId)
+
+      // if (mySelection) editor.selection = mySelection.selection
+
+      // setSelections(otherSelections)
+      // updateEditorState(doc.data.children)
+      // syncMutex.current = false
+    }
+
+    doc.subscribe(onSubscribe)
+    comp.subscribe(onCompSubscribe)
+    doc.on('op', onOperation)
+    comp.on('op', onCompOperation)
+  }, [])
+  
 
   const createComponent = () => {
     const { selectedNodeIds } = editor
@@ -136,7 +216,56 @@ export default function App() {
     []
   )
 
-  const onEditorChange = (value) => {
+  const sendOpToDoc = args => new Promise(resolve => doc.submitOp(args, resolve))
+
+  const sendOpToComp = args => new Promise(resolve => comp.submitOp(args, resolve))
+
+  const onEditorChange = (newValue) => {
+    oldValue.current = { selections: oldSelection.current, children: editorState }
+
+    const selections = oldSelection
+      .current
+      .map(selection => selection.id === userId
+        ? { id: userId, selection: editor.selection, color: '#000', name: 'name' }
+        : selection)
+
+    const diff = jsondiff(oldValue, { selections, children: newValue })
+    oldSelection.current = selections
+
+    if (editor.selection) {
+      console.log(editor.selection)
+      const { focus: { path } } = editor.selection
+      const node = editor
+        .children[path[0]]
+        .children[path[1]]
+
+      if (node.type === 'component') {
+        const oldNode = oldValue.current.children[path[0]].children[path[1]]
+        if (oldNode.type !== 'component') {
+          sendOpToDoc(diff)
+        } else {
+          const componentDiff = jsondiff({ current: comp.data.children }, { selections, children: node.children })
+          if (!syncMutex.current && Array.isArray(componentDiff) && componentDiff.length) {
+            sendOpToComp(componentDiff)
+          }
+        }
+
+        console.log('edit in comp')
+        
+      } else {
+        if (!syncMutex.current && Array.isArray(diff) && diff.length) {
+          sendOpToDoc(diff)
+        }
+      }
+    } else {
+      if (!syncMutex.current && Array.isArray(diff) && diff.length) {
+        sendOpToDoc(diff)
+      }
+    }
+    
+  }
+
+  const onEditorChangee = (value) => {
     if (editor.selection) {
       console.log(editor.selection)
       const { focus: { path } } = editor.selection
